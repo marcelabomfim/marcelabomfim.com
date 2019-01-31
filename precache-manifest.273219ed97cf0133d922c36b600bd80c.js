65a44ea2b02a3c9df091d39649e37ecb4384d109
Marcela Bomfim <mbomfim@live.com>
2019-01-31 20:49:14 -0200

    Updates

---
+++ precache-manifest.273219ed97cf0133d922c36b600bd80c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f471a6e523cc49b478a1904d9192e182",
+    "url": "/static/media/star-on.f471a6e5.svg"
+  },
+  {
+    "revision": "a85894e334844248a204",
+    "url": "/static/js/main.a85894e3.chunk.js"
+  },
+  {
+    "revision": "2078daeeccedb243d41baefcf042668d",
+    "url": "/static/media/sweethomepet.2078daee.jpg"
+  },
+  {
+    "revision": "1ba77fc24c6cabf7a48bc7af883d2a1b",
+    "url": "/static/media/logo.1ba77fc2.svg"
+  },
+  {
+    "revision": "e64448c301b62ed8e614a367ed137e5a",
+    "url": "/static/media/linkedin.e64448c3.svg"
+  },
+  {
+    "revision": "b7382bb0d815d07473e955601adfc6b9",
+    "url": "/static/media/twitter.b7382bb0.svg"
+  },
+  {
+    "revision": "7fc5a16ef5b75054fe8606622e98c26a",
+    "url": "/static/media/github.7fc5a16e.svg"
+  },
+  {
+    "revision": "aa5e7f11941ad05cbb4e5ce2053baf36",
+    "url": "/static/media/bg.aa5e7f11.jpg"
+  },
+  {
+    "revision": "2d72a6249aa96559b1691b3069a38cbc",
+    "url": "/static/media/photo.2d72a624.png"
+  },
+  {
+    "revision": "1cc38ccaf4b07acc4ca11e52f24b07ce",
+    "url": "/static/media/star.1cc38cca.svg"
+  },
+  {
+    "revision": "8d23c6084bb2ef4c49ef",
+    "url": "/static/js/runtime~main.8d23c608.js"
+  },
+  {
+    "revision": "b2e761a2c760d625dddabe983dd953ac",
+    "url": "/static/media/bg7.b2e761a2.jpg"
+  },
+  {
+    "revision": "9e51802d6c68c97c5886542089d06e8a",
+    "url": "/static/media/lojaskd.9e51802d.jpg"
+  },
+  {
+    "revision": "413b1501eddb958c5c68086e2a7f3e28",
+    "url": "/static/media/smartmei.413b1501.jpg"
+  },
+  {
+    "revision": "ec75f1f49baead10f7155882198bfff7",
+    "url": "/static/media/taticaweb.ec75f1f4.jpg"
+  },
+  {
+    "revision": "d02d046992ea026091a66b76f999c334",
+    "url": "/static/media/ubm.d02d0469.jpg"
+  },
+  {
+    "revision": "7d3f2ed5820406c07118bcdcbeac1c65",
+    "url": "/static/media/codepen.7d3f2ed5.jpg"
+  },
+  {
+    "revision": "a200bbb0d6d83bdc5a8d745cb10b09f2",
+    "url": "/static/media/maisabracos.a200bbb0.jpg"
+  },
+  {
+    "revision": "25a752aec4ddc7cb57a68e43d88e41b5",
+    "url": "/static/media/nytt.25a752ae.jpg"
+  },
+  {
+    "revision": "3a8ca3875da6b59d7c87",
+    "url": "/static/js/2.3a8ca387.chunk.js"
+  },
+  {
+    "revision": "c1b4f71721fef86eafd5af3d387dccd3",
+    "url": "/index.html"
+  }
+];
